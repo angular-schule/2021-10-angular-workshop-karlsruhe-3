8c9391e1ea4512371d9d8be9438781487588652a
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2021-10-12 12:00:23 +0200

    routing: going the "snapshot" way

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'br-book-details',
   templateUrl: './book-details.component.html',
   styleUrls: ['./book-details.component.scss']
 })
-export class BookDetailsComponent implements OnInit {
+export class BookDetailsComponent {
 
-  constructor() { }
+  isbn?: string;
 
-  ngOnInit(): void {
+  constructor(private route: ActivatedRoute) {
+
+    this.isbn = this.route.snapshot.paramMap.get('isbn')!;
+
   }
+
 
 }
