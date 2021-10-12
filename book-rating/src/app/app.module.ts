e966305c7ce2a49ae225a8319336f77eac4136a4
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2021-10-12 11:36:57 +0200

    routing: eager loading

--- book-rating/src/app/app.module.ts
+++ book-rating/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,7 +23,9 @@ import { ApiModule, Configuration } from './books/shared/http';
     HttpClientModule,
     ApiModule.forRoot(() => new Configuration({
       basePath: 'https://api.angular.schule'
-    }))
+    })),
+    // LastAppRoutingModule (mit CatchAll drinne) ODER
+    // RouterModule.forChild([{ path: '**', component: NotFoundComponent }])
   ],
   bootstrap: [AppComponent]
 })
--- book-rating/src/app/books/books-routing.module.ts
+++ book-rating/src/app/books/books-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookDetailsComponent } from './book-details/book-details.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'books', component: DashboardComponent },
+  { path: 'books/:isbn', component: BookDetailsComponent }
+    // { path: '**', component: NotFoundComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
