b2e35f70cc3ce5c29f921cab84d3edd08d22fa13
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2021-10-13 11:40:39 +0200

    rxjs: async

--- rxjs-playground/src/app/exercises/unsubscribe/unsubscribe.component.ts
+++ rxjs-playground/src/app/exercises/unsubscribe/unsubscribe.component.ts
@@ -1,52 +1,15 @@
-import { Component, OnDestroy } from '@angular/core';
-import { Subject, ReplaySubject, timer, Subscription } from 'rxjs';
-import { takeWhile, takeUntil, take } from 'rxjs/operators';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { timer } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'rxw-unsubscribe',
   templateUrl: './unsubscribe.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class UnsubscribeComponent implements OnDestroy {
+export class UnsubscribeComponent {
 
-  logStream$ = new ReplaySubject<string | number>();
-  destroy$ = new Subject();
-
-  /**
-   * Öffne die Browser-Console: Dort siehst Du den Output eines Observables, das jede Sekunde einen Wert generiert.
-   * Navigiere zurück auf die Startseite und beobachte die Console:
-   * Die Subscription läuft weiter. Wir haben einen klassischen Memory Leak erzeugt...
-   *
-   * Sorge dafür, dass die Subscription in der Methode ngOnDestroy() beendet wird!
-   * Sie wird beim Buttonklick und beim Wegnavigieren ausgelöst.
-   *
-   * Es gibt noch weitere Wege, das Problem zu lösen...
-   */
-  constructor() {
-    const interval$ = timer(0, 1000);
-
-
-    interval$.pipe(
-
-      /******************************/
-
-      take(3)
-
-      /******************************/
-
-    ).subscribe({
-      next: e => this.log(e),
-      error: err => this.log('❌ ERROR: ' + err),
-      complete: () => this.log('✅ COMPLETE')
-    });
-  }
-
-  ngOnDestroy() {
-    this.logStream$.next('DESTROY');
-    this.destroy$.next();
-  }
-
-  log(msg: string | number) {
-    console.log(msg);
-    this.logStream$.next(msg);
-  }
+  interval$ = timer(0, 1000).pipe(
+    tap(console.log)
+  );
 }
