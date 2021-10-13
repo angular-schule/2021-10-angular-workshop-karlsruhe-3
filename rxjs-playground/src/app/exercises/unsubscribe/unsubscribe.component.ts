4896fed9deceb14cda5185b6801efd250ea418e5
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2021-10-13 11:26:40 +0200

    rxjs: takeUntil

--- rxjs-playground/src/app/exercises/unsubscribe/unsubscribe.component.ts
+++ rxjs-playground/src/app/exercises/unsubscribe/unsubscribe.component.ts
@@ -9,7 +9,7 @@ import { takeWhile, takeUntil } from 'rxjs/operators';
 export class UnsubscribeComponent implements OnDestroy {
 
   logStream$ = new ReplaySubject<string | number>();
-  destroyed = false;
+  destroy$ = new Subject();
 
   /**
    * Öffne die Browser-Console: Dort siehst Du den Output eines Observables, das jede Sekunde einen Wert generiert.
@@ -29,7 +29,7 @@ export class UnsubscribeComponent implements OnDestroy {
 
       /******************************/
 
-      takeWhile(() => !this.destroyed)
+      takeUntil(this.destroy$)
 
       /******************************/
 
@@ -42,7 +42,7 @@ export class UnsubscribeComponent implements OnDestroy {
 
   ngOnDestroy() {
     this.logStream$.next('DESTROY');
-    this.destroyed = true;
+    this.destroy$.next();
   }
 
   log(msg: string | number) {
