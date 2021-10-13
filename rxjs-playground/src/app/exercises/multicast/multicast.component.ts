d0f6cc2ca5c0a09015544f394dac998fbb4c8695
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2021-10-13 11:09:07 +0200

    rxjs: BehaviourSubject

--- rxjs-playground/src/app/exercises/multicast/multicast.component.ts
+++ rxjs-playground/src/app/exercises/multicast/multicast.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Subject, BehaviorSubject, ReplaySubject, Observable } from 'rxjs';
-import { share, shareReplay } from 'rxjs/operators';
+import { delay, share, shareReplay } from 'rxjs/operators';
 
 import { MeasureValuesService } from './measure-values.service';
 import { ExerciseService } from '../exercise.service';
@@ -14,16 +14,22 @@ export class MulticastComponent {
   listeners: string[] = [];
   logStream$ = new ReplaySubject<string>();
 
-  measureValues$: Observable<number>; // später: Subject<number>;
+  measureValues$: Subject<number>; // später: Subject<number>;
 
   constructor(private mvs: MeasureValuesService, private es: ExerciseService) {
 
     /**************!!**************/
 
-    this.measureValues$ = this.mvs.getValues().pipe(
-      // share(),
-      shareReplay(1)
-    )
+    // this.measureValues$ = this.mvs.getValues().pipe(
+    //   // share(),
+    //   shareReplay(1)
+    // )
+
+    this.measureValues$ = new BehaviorSubject(9999);
+    this.mvs.getValues().pipe(
+      delay(3000)
+    ).subscribe(this.measureValues$)
+
     /**************!!**************/
 
   }
